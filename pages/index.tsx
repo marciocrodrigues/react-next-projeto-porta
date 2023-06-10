b132a1430ad75eb7f91d8e07384c23aeaa3c9dce
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2023-06-10 00:17:19 -0300

    componente porta, inclusao typescript

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import {useState} from "react";
+import Porta from "../components/Porta";
+import Presente from "../components/Presente";
+import PortaModel from "../model/porta";
+
+export default function Home() {
+  const [p1, setP1] = useState(new PortaModel(1))
+
+  return (
+    <div style={{
+      display: 'flex'
+    }}>
+      <Porta value={p1} />
+    </div>
+  )
+}
